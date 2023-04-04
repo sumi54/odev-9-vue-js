ccc0b2969acc54affd6dcb21a945dbabd0ef1083
sumi54 <sumeyyekarakullukcu34@gmail.com>
2023-04-04 11:48:30 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,91 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+      <form v-if="!submited">
+          <div class="row" >
+              <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                  <!-- Soru 1 -->
+                  <!-- Aşağıdaki bilgileri içeren bir Üye Ol sayfası geliştiriniz. -->
+                  <!-- Tam Adı (Ad + Soyad) -->
+                  <!-- E-mail -->
+                  <!-- Şifre -->
+                  <!-- Cinsiyet (Kadın/Erkek) -->
+                  <!-- <div class="form-group">
+                    <label>Ad:</label>
+                    <input type="text" class="form-control" v-model="info.name">
+                  </div>
+                  <div class="form-group">
+                    <label>Soyad:</label>
+                    <input type="text" class="form-control" v-model="info.surname">
+                  </div> -->
+                 <appfullName v-model="info.fullname"></appfullName>
+                  <div class="form-group">
+                    <label>E-mail:</label>
+                    <input type="text" class="form-control" v-model="info.mail">
+                  </div>
+                  <div class="form-group">
+                    <label>Şifre:</label>
+                    <input type="password" class="form-control" v-model="info.password">
+                  </div>
+                  <div class="form-group">
+                    <label>
+                        <input type="radio" v-model="info.gender" value="kadin">Kadın
+                    </label>
+                    <label>
+                        <input type="radio" v-model="info.gender" value="erkek">Erkek
+                    </label>
+                  </div>
+                  <button @click.prevent="submit" class="btn btn-primary">Kayıt ol</button>
+                  <!-- Soru 2 -->
+                  <!-- Üye Ol formu, Submit EDİLMEDİĞİ SÜRECE ekranda gözükmelidir.-->
+                  <!-- Üye Bilgileri Ekranı ise form Submit EDİLDİKTEN SONRA ekranda gözükecektir -->
+
+                  <!-- Soru 3 -->
+                  <!-- Yukarıdaki uygulamayı düzenleyerek Tam Ad (Ad + Soyad) alanı için bir Form Kontrol Elemanı üretiniz -->
+              </div>
+          </div>
+      </form>
+      <div class="row" v-else>
+          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+              <div class="panel panel-info">
+                  <div class="panel-heading">
+                      <h4>Girdiğiniz Bilgier</h4>
+                  </div>
+                  <div class="panel-body">
+                      <p>Tam Ad:   {{info.fullname}}</p>
+                      <p>E-mail:   {{info.mail}}</p>
+                      <p>Şifre:    {{info.password}}</p>
+                      <p>Cinsiyet: {{info.gender}}</p>
+                  </div>
+              </div>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+import Fullname from "./fullName.vue";
+  export default {
+    data(){
+        return{
+            info:{
+            fullname:"Süm Kar",
+            mail:"",
+            password:"",
+            gender:""
+            },
+            submited:false
+        }
+    },
+    methods:{
+        submit(){
+            this.submited=true;
+        }
+    },
+    components:{
+      appfullName:Fullname
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
---
+++ src/fullName.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+         <div class="form-group">
+            <label>Ad:</label>
+            <input type="text" class="form-control" :value="name" @input="nameChange(true,$event)">
+          </div>
+          <div class="form-group">
+            <label>Soyad:</label>
+            <input type="text" class="form-control" :value="surname" @input="nameChange(false,$event)">
+          </div>
+     </div>
+</template>
+<script>
+export default{
+  props:["value"],
+  computed:{
+    name(){
+      return this.value.split(" ")[0]
+    },
+    surname(){
+      return this.value.split(" ")[1]
+    }
+  },
+  methods:{
+    nameChange(isfirst,event){
+      let fullName="";
+      if(isfirst){
+        fullName=event.target.value+" "+this.surname;
+      }else{
+        fullName=this.name+" "+event.target.value
+      }
+      this.value=fullName;
+      this.$emit("input",this.value)
+    }
+  }
+}
+</script>
